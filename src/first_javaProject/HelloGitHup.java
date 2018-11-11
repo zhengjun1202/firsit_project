1788904e326c12de3ec68957771508df94838e8b
郑军 <zj749122147@outlook.com>
2018-11-11 16:08:12 +0800

    上传我的第一个gitHup项目

---
+++ src/first_javaProject/HelloGitHup.java
@@ -0,0 +1,12 @@
+package first_javaProject;
+
+public class HelloGitHup {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("欢迎，来到郑军的个人gitHup");
+		System.out.println("这是郑军的个人的gitHup账号");
+		
+	}
+	
+}
